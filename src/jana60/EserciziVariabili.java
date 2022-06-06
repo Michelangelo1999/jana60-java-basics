da41f8f3f7d8bb61489155dd661e8fd43db27cb7
Michelangelo1999 <mikidenicola007@gmail.com>
2022-06-06 09:43:37 +0200

    fisrt commit

---
+++ src/jana60/EserciziVariabili.java
@@ -0,0 +1,12 @@
+package jana60;
+
+public class EserciziVariabili {   //il nome della class deve essere uguale al nome del filr
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//esercizi con le variabili
+		
+
+	}
+
+}
